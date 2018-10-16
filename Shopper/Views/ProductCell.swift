5d66c4ff2c403bb530d6036d97b2f49a1f5497d1
Artur Ratajczak <artur.ratajczak1@gmail.com>
2018-10-16 11:21:23 -0700

    products collectionView Ui

---
+++ Shopper/Views/ProductCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProductCell.swift
+//  Shopper
+//
+//  Created by Artur on 16/10/2018.
+//  Copyright © 2018 Artur. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
